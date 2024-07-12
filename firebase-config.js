d3ac1997bf448b4641a02cba16f0dfeeae4179a6
Leo Celestine <leocelestine@Leos-MacBook-Pro.local>
2024-07-12 17:27:23 +0530

    Updated Firebase initialization with script tags for CDN

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,48 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/8.6.8/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/8.6.8/firebase-database.js";
+document.addEventListener('DOMContentLoaded', () => {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBwSh1EQ3rrGrXQCLbW046SacohLgMwgrk",
+        authDomain: "geoatkt.firebaseapp.com",
+        databaseURL: "https://geoatkt-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "geoatkt",
+        storageBucket: "geoatkt.appspot.com",
+        messagingSenderId: "410244661766",
+        appId: "1:410244661766:web:481cc95b80bcf3a885b987",
+        measurementId: "G-C9HSEVJSY5"
+    };
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBwSh1EQ3rrGrXQCLbW046SacohLgMwgrk",
-    authDomain: "geoatkt.firebaseapp.com",
-    databaseURL: "https://geoatkt-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "geoatkt",
-    storageBucket: "geoatkt.appspot.com",
-    messagingSenderId: "410244661766",
-    appId: "1:410244661766:web:481cc95b80bcf3a885b987",
-    measurementId: "G-C9HSEVJSY5"
-};
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
+    // Reference for database
+    var attendanceFormDB = firebase.database().ref('attendanceForm');
 
-export { auth, database };
+    document.getElementById("attendanceForm").addEventListener("submit", submitForm);
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        var name = getElementVal('employeeName');
+        var time = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+
+        console.log(name, time);
+
+        saveMessages(name, time);
+
+        // Clear form
+        document.getElementById("attendanceForm").reset();
+    }
+
+    const saveMessages = (name, time) => {
+        var newAttendanceForm = attendanceFormDB.push();
+        newAttendanceForm.set({
+            name: name,
+            time: time,
+        });
+    };
+
+    const getElementVal = (id) => {
+        console.log("Getting value for:", id);  // Debugging log
+        return document.getElementById(id).value;
+    };
+});
